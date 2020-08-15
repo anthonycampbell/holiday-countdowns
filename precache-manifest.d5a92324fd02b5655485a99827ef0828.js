d9d249c2e7bd1a07a994cd3dd88bb39616a2ee11
anthonycampbell <anthony-campbell@hotmail.com>
2020-08-15 02:32:54 -0700

    Updates

---
+++ precache-manifest.d5a92324fd02b5655485a99827ef0828.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8627279c5a9d6544faa02095b24e93",
+    "url": "/holiday-countdowns/index.html"
+  },
+  {
+    "revision": "43fbf70643b2e4b2913b",
+    "url": "/holiday-countdowns/static/css/main.3e3cd716.chunk.css"
+  },
+  {
+    "revision": "889244b009b804a5b98a",
+    "url": "/holiday-countdowns/static/js/2.754b9308.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/holiday-countdowns/static/js/2.754b9308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fbf70643b2e4b2913b",
+    "url": "/holiday-countdowns/static/js/main.47b183e1.chunk.js"
+  },
+  {
+    "revision": "465b9a49261274d9a9be",
+    "url": "/holiday-countdowns/static/js/runtime-main.adc539f8.js"
+  },
+  {
+    "revision": "71c6d8d4c1adcc80df178e529958f39b",
+    "url": "/holiday-countdowns/static/media/cadbury.71c6d8d4.jpg"
+  },
+  {
+    "revision": "103d18d0082b83bb8296fee90b49b67e",
+    "url": "/holiday-countdowns/static/media/gift.103d18d0.jpg"
+  },
+  {
+    "revision": "0a032225305b7f0755504bef49305bb8",
+    "url": "/holiday-countdowns/static/media/kisses.0a032225.jpg"
+  },
+  {
+    "revision": "89d4ff2e4247c0889b09a3671d1a168a",
+    "url": "/holiday-countdowns/static/media/spoopy.89d4ff2e.jpg"
+  }
+]);
